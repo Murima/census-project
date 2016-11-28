cfc747396edfd4cacaea59473f65ea814774fb3d
Murima <elvimash@gmail.com>
2016-11-28 19:05:50 +0800

    validate signin

--- resources/views/loginpage.blade.php
+++ resources/views/loginpage.blade.php
@@ -2,6 +2,19 @@
 
 @section('content')
 <h2> Sign In</h2>
+@if (count ($errors)>0)
+
+    <div class="alert alert-danger">
+<ul>
+    @foreach($errors->all() as $error)
+        <li>{{ $error }}</li>
+
+    @endforeach
+</ul>
+
+    </div>
+    @endif
+
     <div class="row">
 
         <div class="col-md-6">
