1574974ca01103b8408bc19b0aaf91f5aaeb866e
Murima <elvimash@gmail.com>
2016-11-30 09:07:51 +0800

    changed dashboard to blade templating

---
+++ resources/views/includes/dashboard/dashboard-footer.blade.php
@@ -0,0 +1,9 @@
+<!-- Main Footer -->
+<footer class="main-footer">
+    <!-- To the right -->
+    <div class="pull-right hidden-xs">
+        stats
+    </div>
+    <!-- Default to the left -->
+    <strong>Copyright &copy; 2016 <a href="#">Kenya National Bureau of Statistics</a>.</strong> All rights reserved.
+</footer>
